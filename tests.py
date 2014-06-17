34c2591782cf4e901a8abd5350158bf113becc9e
Joshua Ruihley <ruihley@gmail.com>
2014-06-17 10:35:35 -0400

    date tests and test reorg

--- tests.py
+++ tests.py
@@ -8,22 +8,38 @@ class ServerTests(unittest.TestCase):
 	"""tests to ensure pages at dodsbir.net can be reached and lists and topics
 	can be retrieved"""
 
+	def setUp(self):
+		self.s = dodsbirscrape.DODSBIRScrape()
+
 	def testServerResponse(self):
 		self.assertTrue(urllib.urlopen(dodsbirscrape.URL_TOPIC_LIST))
 
 	def testStageSolicitation(self):
-		s = dodsbirscrape.DODSBIRScrape()
-		self.assertFalse(s.topic_ids)
-		s.stage_current_solicitation()
-		self.assertTrue(s.topic_ids)
+		self.assertFalse(self.s.topic_ids)
+		self.s.stage_current_solicitation()
+		self.assertTrue(self.s.topic_ids)
+
+
+class TopicTests(unittest.TestCase):
+	"""tests to ensure Topics are successfully scraped and stored in the proper
+	format"""
+
+	def setUp(self):
+		self.s = dodsbirscrape.DODSBIRScrape()
 
 	def testTopicRetrieved(self):
-		s = dodsbirscrape.DODSBIRScrape()
-		topic = s.get_topic("SB142-004")
-		self.assertTrue(topic.title)
+		self.assertFalse(hasattr(self, "topic"))
+		self.topic = self.s.get_topic("SB142-004")
+		self.assertTrue(self.topic)
+
+	def testDatesConvertedToPythonObject(self):
+		self.topic = self.s.get_topic("SB142-004")
+		self.assertTrue(self.topic.solicitation["pre_release_date"].month)
+		self.assertTrue(self.topic.solicitation["proposals_begin_date"].month)
+		self.assertTrue(self.topic.solicitation["proposals_end_date"].month)
 
 def main():
-    unittest.main()
+    unittest.main()	
 
 if __name__ == '__main__':
     main()
