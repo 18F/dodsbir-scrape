936ead0dda138a8a364f592446bc022812301c17
Joshua Ruihley <ruihley@gmail.com>
2014-06-18 11:40:35 -0400

    packaged

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name="dodsbirscrape",
+	version='0.8',
+	description='Scraping library for DODSBIR.net',
+	url='http://github.com/jroo/dodsbir-scrape',
+	author='18F.gsa.gov',
+	zip_safe=False)
