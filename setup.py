46a526c2b00eb05643d72c31663a153650b684ec
Alison Rowland <alisonrowland@gmail.com>
2014-08-05 13:39:17 -0400

    Updated URL

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name="dodsbir",
 	version='0.9.1',
 	description='Scraping library for DODSBIR.net',
-	url='http://github.com/jroo/dodsbir-scrape',
+	url='http://github.com/18F/dodsbir-scrape',
 	author='18F.gsa.gov',
 	zip_safe=False,
 	install_requires = [
