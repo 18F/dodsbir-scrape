52bc91fa3b7abfcb83fcc19f24230e41330dbea5
arowla <alison.rowland@gsa.gov>
2015-01-28 16:56:45 -0500

    Bringing tests up to date with 2015 solicitations
    - Also fixing flake8 warnings:
      - tabs -> spaces
      - removed trailing whitespace

--- lib/tests.py
+++ lib/tests.py
@@ -5,46 +5,46 @@ from scrape import Scraper, URL_TOPIC_LIST
 
 
 class ServerTests(unittest.TestCase):
-	"""tests to ensure pages at dodsbir.net can be reached and lists and topics
-	can be retrieved"""
+    """tests to ensure pages at dodsbir.net can be reached and lists and topics
+    can be retrieved"""
 
-	def setUp(self):
-		self.s = Scraper()
+    def setUp(self):
+        self.s = Scraper()
 
-	def testServerResponse(self):
-		self.assertTrue(request.urlopen(URL_TOPIC_LIST))
+    def testServerResponse(self):
+        self.assertTrue(request.urlopen(URL_TOPIC_LIST))
 
-	def testStageSolicitation(self):
-		self.assertFalse(self.s.topic_ids)
-		self.s.stage_current_solicitation()
-		self.assertTrue(self.s.topic_ids)
+    def testStageSolicitation(self):
+        self.assertFalse(self.s.topic_ids)
+        self.s.stage_current_solicitation()
+        self.assertTrue(self.s.topic_ids)
 
 
 class TopicTests(unittest.TestCase):
-	"""tests to ensure Topics are successfully scraped and stored in the proper
-	format"""
+    """tests to ensure Topics are successfully scraped and stored in the proper
+    format"""
 
-	def setUp(self):
-		self.s = Scraper()
+    def setUp(self):
+        self.s = Scraper()
 
-	def testTopicRetrieved(self):
-		self.assertFalse(hasattr(self, "topic"))
-		topic = self.s.get_topic("SB142-004")
-		self.assertTrue(topic)
+    def testTopicRetrieved(self):
+        self.assertFalse(hasattr(self, "topic"))
+        topic = self.s.get_topic("SB151-004")
+        self.assertTrue(topic)
 
-	def testTopicSolicitationID(self):
-		topic = self.s.get_topic("SB142-004")
-		self.assertEqual(topic.solicitation_id, "DoD SBIR 2014.2")
+    def testTopicSolicitationID(self):
+        topic = self.s.get_topic("SB151-004")
+        self.assertEqual(topic.solicitation_id, "DoD SBIR 2015.1")
 
-	def testDatesConvertedToPythonObject(self):
-		topic = self.s.get_topic("SB142-004")
-		self.assertTrue(topic.pre_release_date.month)
-		self.assertTrue(topic.proposals_begin_date.month)
-		self.assertTrue(topic.proposals_end_date.month)
+    def testDatesConvertedToPythonObject(self):
+        topic = self.s.get_topic("SB151-004")
+        self.assertTrue(topic.pre_release_date.month)
+        self.assertTrue(topic.proposals_begin_date.month)
+        self.assertTrue(topic.proposals_end_date.month)
 
 
 def main():
-    unittest.main()	
+    unittest.main()
 
 if __name__ == '__main__':
     main()
